7d82f34a6ef2104274501949b11669c17038aac3
Deejayayay <122236585+Deejayayay@users.noreply.github.com>
2024-11-01 01:28:24 -0700

    started projects page

--- dj-portfolio/src/pages/projects.tsx
+++ dj-portfolio/src/pages/projects.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 
 import '../styles/projects.css'
 
@@ -6,6 +5,23 @@ const Projects: React.FC = () => {
     return(
         <div className='project-container'>
             <h1>Projects</h1>
+            <div className="bento">
+                <div className="row row-1">
+
+                    <div className="col">
+                        <div className="col-box"></div>
+                        <div className="col-box"></div>
+                    </div>
+
+                    <div className="col col-2">
+                        <div className="col-box"></div>
+                        <div className="col-box"></div>
+                    </div>
+                </div>
+                <div className="row row-1"></div>
+                <div className="row row-1"></div>
+            </div>
+
         </div>
     );
 };
